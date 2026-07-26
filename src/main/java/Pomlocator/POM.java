b6847334e81b70db55a96c6e9232acefc4b215d8
Manojpabu <manojabi2267@gmail.com>
2026-07-26 22:16:25 +0530

    third commit

---
+++ src/main/java/Pomlocator/POM.java
@@ -0,0 +1,17 @@
+package Pomlocator;
+
+import org.openqa.selenium.By;
+
+public class POM {
+	
+	public By home = By.xpath("//i[@class=\"fa fa-home\"]");
+
+	public By sign = By.xpath("//i[@class=\"fa fa-lock\"]");
+
+	public By name = By.xpath("//input[@name=\"name\"]");
+
+	public By email = By.xpath("//input[@data-qa=\"signup-email\"]");
+		
+
+
+}
